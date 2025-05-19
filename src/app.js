24c8bb8f92353e2d598e48f8abe7baddd6d7611b
Incerteza88 <info@incerteza.es>
2025-05-19 18:56:37 +0000

    Primeros cambios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const pronombres = ["el", "mi", "tu"];
+const adjetivos = ["super", "grande", "rápido"];
+const sustantivos = ["perro", "gato", "coche"];
+const dominios = [".com", ".net", ".org"];
+
+window.onload = () => {
+  const boton = document.getElementById("boton-generar");
+  const lista = document.getElementById("lista-dominios");
+
+  boton.addEventListener("click", () => {
+    // Elegir uno aleatorio de cada array
+    const pron = pronombres[Math.floor(Math.random() * pronombres.length)];
+    const adj = adjetivos[Math.floor(Math.random() * adjetivos.length)];
+    const sust = sustantivos[Math.floor(Math.random() * sustantivos.length)];
+    const dom = dominios[Math.floor(Math.random() * dominios.length)];
+
+    const dominio = pron + adj + sust + dom;
+
+    // Mostrarlo en la lista
+    const li = document.createElement("li");
+    li.className = "list-group-item";
+    li.textContent = dominio;
+    lista.appendChild(li);
+  });
 };
